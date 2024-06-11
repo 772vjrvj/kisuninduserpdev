00a138262e79674f1672cab643b6446436ff941f
고승현 <772vjrvj@naver.com>
2024-06-11 22:53:33 +0900

    g
    g

---
+++ src/main/java/com/erp/kisunindus/KisunindusApplication.java
@@ -0,0 +1,13 @@
+package com.erp.kisunindus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisunindusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KisunindusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/kisunindus/KisunindusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.kisunindus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KisunindusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
